1a7a409ea9ce887bb8d34484c94869c4fbbb7383
j777y <tojerryjoseph@gmail.com>
2016-05-07 17:30:20 +0530

    sdfsdf

---
+++ main.py
@@ -0,0 +1,21 @@
+from auto import auto_follow
+from auto import auto_fav
+
+#this is a sample program to use the auto.py script
+
+auto_fav("lol", count=1)
+auto_fav("funny", count=1)
+auto_fav("humor", count=1)
+auto_fav("joke", count=1)
+auto_fav("omg", count=1)
+auto_fav("amusing", count=1)
+
+#auto follow 10 users who come when we search for keyword "followback"
+
+auto_follow("lol", count=1)
+auto_follow("funny", count=1)
+auto_follow("omg", count=1)
+auto_follow("joke", count=10)
+auto_follow("amusing", count=10)
+auto_follow("hot", count=10)
+
