8ddf6daff47f67b32ca417ec40b73811c28d80c7
j777y <tojerryjoseph@gmail.com>
2016-05-24 10:44:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,19 @@ from auto import auto_fav
 
 #this is a sample program to use the auto.py script
 
-auto_fav("lol", count=5)
-auto_fav("funny", count=5)
-auto_fav("humor", count=5)
-auto_fav("joke", count=5)
-auto_fav("omg", count=5)
-auto_fav("amusing", count=5)
+auto_fav("lol", count=1)
+auto_fav("funny", count=1)
+auto_fav("humor", count=1)
+auto_fav("joke", count=1)
+auto_fav("omg", count=1)
+auto_fav("amusing", count=1)
 
 #auto follow 10 users who come when we search for keyword "followback"
 
-auto_follow("lol", count=5)
-auto_follow("funny", count=5)
-auto_follow("omg", count=5)
-auto_follow("joke", count=5)
-auto_follow("amusing", count=5)
-auto_follow("hot", count=5)
+auto_follow("lol", count=1)
+auto_follow("funny", count=1)
+auto_follow("omg", count=1)
+auto_follow("joke", count=1)
+auto_follow("amusing", count=1)
+auto_follow("humor", count=1)
 
