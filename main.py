ab1bc8f073719516f34553ea9418c45f7e9487f7
j777y <tojerryjoseph@gmail.com>
2016-05-26 18:30:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ import time
 
 auto_fav("lol", count=1)
 auto_fav("funny", count=1)
+auto_fav("amusing", count=1)
+auto_fav("beautiful", count=1)
 
 #auto follow 10 users who come when we search for keyword "followback"
 
 auto_follow("lol", count=5)
 auto_follow("funny", count=5)
+auto_follow("amusing", count=1)
+auto_follow("beautiful", count=1)
